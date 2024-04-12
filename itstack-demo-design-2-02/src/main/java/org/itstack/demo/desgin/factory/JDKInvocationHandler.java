4664f7851419cee3ae5b986f4ec522a196d81b75
wanggang <wanggang@come-future.com>
2024-04-12 10:53:54 +0800

    动态代理

--- itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKInvocationHandler.java
+++ itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKInvocationHandler.java
@@ -1,20 +1,40 @@
 package org.itstack.demo.desgin.factory;
 
+import org.itstack.demo.desgin.CacheService;
 import org.itstack.demo.desgin.util.ClassLoaderUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 public class JDKInvocationHandler implements InvocationHandler {
 
     private ICacheAdapter cacheAdapter;
 
+    private Object object;
+
     public JDKInvocationHandler(ICacheAdapter cacheAdapter) {
         this.cacheAdapter = cacheAdapter;
     }
 
+    public JDKInvocationHandler(Object object) {
+        this.object = object;
+    }
+
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
+        System.out.println("哈哈");
+        //return ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
+        //ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
+        //Object res = proxy.getClass().getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(proxy, args);
+        //Object res = method.invoke(proxy, args);
+        Object res;
+        if (Objects.isNull(object)) {
+            res = ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
+        }else {
+            res = method.invoke(object, args);
+        }
+        System.out.println("哈哈2");
+        return res;
     }
 
 }
--- itstack-demo-design-2-02/src/test/java/org/itstack/demo/desgin/test/ApiTest.java
+++ itstack-demo-design-2-02/src/test/java/org/itstack/demo/desgin/test/ApiTest.java
@@ -18,6 +18,12 @@ public class ApiTest {
         String val01 = proxy_EGM.get("user_name_01");
         System.out.println("测试结果：" + val01);
 
+        //这是个动态生成的代理对象，没有代码上的具体实现？
+        CacheService proxy_EGM2 = JDKProxy.getProxy(CacheServiceImpl.class, new CacheServiceImpl());
+        proxy_EGM2.set("user_name_01", "小傅哥");
+        String val012 = proxy_EGM2.get("user_name_01");
+        System.out.println("测试结果：" + val012);
+
         CacheService proxy_IIR = JDKProxy.getProxy(CacheServiceImpl.class, new IIRCacheAdapter());
         proxy_IIR.set("user_name_01", "小傅哥");
         String val02 = proxy_IIR.get("user_name_01");
